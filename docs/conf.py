bb5d2189c2d3bfab2136bfc6d8d77cdaf2aa6b1d
hamolicious <kickfliptv8@gmail.com>
2023-05-30 10:58:53 +0100

    changed to underscores

--- docs/conf.py
+++ docs/conf.py
@@ -32,5 +32,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'renku-sphinx-theme'
+html_theme = 'renku_sphinx_theme'
 html_static_path = ['_static']
