0d5804f6b95ad5eb0b767055305c09c856dfb0ad
hamolicious <kickfliptv8@gmail.com>
2023-05-30 11:27:40 +0100

    added theme

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
+    'renku_sphinx_theme',
 ]
 
 templates_path = ['_templates']
@@ -32,5 +32,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'renku'
 html_static_path = ['_static']
